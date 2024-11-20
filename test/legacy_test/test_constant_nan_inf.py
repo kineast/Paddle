5d16d684bb042b0d09821e4902cc4a653175372b
kineast <2378471710@qq.com>
2024-11-20 05:56:30 +0000

    test&init

---
+++ test/legacy_test/test_constant_nan_inf.py
@@ -0,0 +1,27 @@
+import math
+import paddle
+import unitest
+import numpy as np
+
+class TestConstantNanInf(unitest.TestCase):
+
+    def TestConstantNan(self):
+        #self.assertEqual(math.nan, paddle_nan)
+        x = np.array([paddle.nan])
+        np.testing.assert_equal(repr(x), 'array([nan])')
+    
+    def TestConstantInf(self):
+        x = np.array([paddle.inf])
+        np.testing.assert_equal(repr(x), 'array([inf])')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+
+
+
+
+

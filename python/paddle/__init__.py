6c1c6075db310abda11c7ddb101e715313b0282c
kineast <2378471710@qq.com>
2024-11-21 02:49:39 +0000

    test

--- python/paddle/__init__.py
+++ python/paddle/__init__.py
@@ -776,7 +776,6 @@ ir_guard._switch_to_pir()
 # Constants
 inf = math.inf
 nan = math.nan
-newaxis: None = None
 
 __all__ = [
     'block_diag',
--- test/legacy_test/test_constant_nan_inf.py
+++ test/legacy_test/test_constant_nan_inf.py
@@ -18,16 +18,12 @@ import unitest
 import paddle
 
 
-class TestConstantNanInf(unitest.TestCase):
+class TestConstantNan(unitest.TestCase):
 
     def TestConstantNan(self):
         # self.assertEqual(math.nan, paddle_nan)
         x = np.array([paddle.nan])
         np.testing.assert_equal(repr(x), 'array([nan])')
-
-    def TestConstantInf(self):
-        x = np.array([paddle.inf])
-        np.testing.assert_equal(repr(x), 'array([inf])')
 
 
 if __name__ == '__main__':
